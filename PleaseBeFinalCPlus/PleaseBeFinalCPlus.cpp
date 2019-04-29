2149042a35da69607a65f8369ca1abbbd51bca9b
danykivanov <danykivanov@gmail.com>
2019-04-29 22:20:18 +0300

    Add project files.

---
+++ PleaseBeFinalCPlus/PleaseBeFinalCPlus.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+#include <string.h>
+using namespace std;
+//using std::
+int main()
+{
+	char a[2];
+	int res = 1;
+
+	for (int i = 0; i < 2; i++) {
+		cin >> a;
+		res = res * atoi(a);
+	}
+	
+	cout << " Res = " << res;
+
+	return 0;
+}
